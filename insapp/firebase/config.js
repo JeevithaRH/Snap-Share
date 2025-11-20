23140452852cf39618301ab5a2b490eaaff891cc
JeevithaRH <jeevithagowdarh@gmail.com>
2025-11-20 15:48:08 +0530

    change simply

--- insapp/firebase/config.js
+++ insapp/firebase/config.js
@@ -4,18 +4,24 @@ import { initializeApp } from "firebase/app";
 import {
   initializeAuth,
   getReactNativePersistence
-} from "firebase/auth";
+} from "firebase/remote-config";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  // apiKey: "YOUR_KEY",
+  // authDomain: "YOUR_DOMAIN",
+  // projectId: "YOUR_PROJECT_ID",
+  // storageBucket: "YOUR_BUCKET",
+  // messagingSenderId: "YOUR_SENDER_ID",
+  // appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA6RPjVElKcsV8GBO4YJNsZ7NSJG7Zu3gA",
+  authDomain: "snapshare-6d06b.firebaseapp.com",
+  projectId: "snapshare-6d06b",
+  storageBucket: "snapshare-6d06b.firebasestorage.app",
+  messagingSenderId: "339302956210",
+  appId: "1:339302956210:web:c99604be29a48fe711ddab"
 };
 
 const app = initializeApp(firebaseConfig);
